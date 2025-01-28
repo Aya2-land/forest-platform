4de1d422c9c22e4c6e21a39088c378a2d8f35298
OOkeychanOO <146065530+OOkeychanOO@users.noreply.github.com>
2025-01-28 10:38:40 +0900

    モードチェンジ

--- js/select_mode.js
+++ js/select_mode.js
@@ -7,9 +7,12 @@ document.getElementById("selectmodebutton").addEventListener('click', function()
             console.log("1")
             window.location.href = "kagitani-system/select_sheet.php";
           }else if(selectmodeoption[i].value === "2"){
+            // ここ変える必要あり
+            window.location.href = "forest-mrn/select_sheet.php";
+          }else if(selectmodeoption[i].value === "3"){
             console.log("2")
             window.location.href = "kii-system/select_sheet.php";
-          }else if(selectmodeoption[i].value === "3"){
+          }else if(selectmodeoption[i].value === "4"){
             // ここ変える必要あり
             window.location.href = "fujinaka-system/select_sheet.php";
           }
