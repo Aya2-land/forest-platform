133e5aec1402075238254e750af7eee326fa37d9
OOkeychanOO <renarenaforopu@gmail.com>
2025-01-28 17:13:54 +0900

    version系修正

--- forest-mrn/js/version_update.js
+++ forest-mrn/js/version_update.js
@@ -97,7 +97,7 @@ function NodeVersionUpdate(nodes){
              if(!res || res){
               console.log(res);
              }
-             GetPairNodeId_ContentRelationTable(nodeID);
+            //  GetPairNodeId_ContentRelationTable(nodeID);
           },
           error: function () {
             console.log("node_versionsに保存失敗");
@@ -131,7 +131,7 @@ function NodeVersionUpdate(nodes){
          if(res){
           console.log(res);
          }
-         GetPairNodeId_ContentRelationTable(nodeID);
+        //  GetPairNodeId_ContentRelationTable(nodeID);
       },
       error: function () {
         console.log("node_versionsに保存失敗");
