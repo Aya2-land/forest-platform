85e282af6cc92d71dd88ef3199a2eecb5cc3b638
sayakakawa <146063396+sayakakawa@users.noreply.github.com>
2025-04-11 16:03:58 +0900

    Revert "DB(サーバー用)"
    This reverts commit aad4b594e9219ff76f52d1dbc5a27e46d6cb0289.

--- forest-mrn/php/connect_db.php
+++ forest-mrn/php/connect_db.php
@@ -1,18 +1,17 @@
 <?php
 
 	
-	// $db_host = "10.240.187.201";  // ホスト名のみ
-	// $db_port = 8889;  // ポート番号
-	// $db_user = "root";
-	// $db_password = "your_password";
-	// $db_dbname = "forest_platform";
+	$db_host = "10.240.187.201";  // ホスト名のみ
+	$db_port = 8889;  // ポート番号
+	$db_user = "root";
+	$db_password = "your_password";
+	$db_dbname = "forest_platform";
 
 	//アプリケーションサーバーにアップロードするとき．
-	$db_host = "192.168.0.82";  // DBサーバのurl
-	$db_port = 3306;
-	$db_user = "root";
-	$db_password = "kslabkslab";
-	$db_dbname = "forest-platform";
+	// $db_host = "localhost:3306";  // DBサーバのurl
+	// $db_user = "root";
+	// $db_password = "kslabkslab";
+	// $db_dbname = "shimizu";
 
 	// mysqlへの接続
 	$mysqli = new mysqli($db_host, $db_user, $db_password, $db_dbname, $db_port);
