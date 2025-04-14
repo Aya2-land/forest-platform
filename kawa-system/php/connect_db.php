7b9c0950d960c50dbb0f24f0d5cc5ff28f3f6b78
sayakakawa <146063396+sayakakawa@users.noreply.github.com>
2025-04-14 16:50:31 +0900

    Reapply "Reapply "DB(サーバー用)""
    This reverts commit 88e26cb2a42703d8ad7b7efe8bffd2e792d3caf9.

--- kawa-system/php/connect_db.php
+++ kawa-system/php/connect_db.php
@@ -1,16 +1,18 @@
 <?php
 
-	$db_host = "10.240.187.201";  // ホスト名のみ
-	$db_port = 8889;  // ポート番号
-	$db_user = "root";
-	$db_password = "your_password";
-	$db_dbname = "forest_platform";
+	//kagitani共有
+	// $db_host = "10.240.187.201";  // ホスト名のみ
+	// $db_port = 8889;  // ポート番号
+	// $db_user = "root";
+	// $db_password = "your_password";
+	// $db_dbname = "forest_platform";
 
 	// 　統合環境1
-	// $db_host = "192.168.0.82:3306";  // DBサーバのurl
-	// $db_user = "root";
-	// $db_password = "kslabkslab";
-	// $db_dbname = "nishida2";
+	$db_host = "192.168.0.82";  // DBサーバのurl
+	$db_port = 3306;
+	$db_user = "root";
+	$db_password = "kslabkslab";
+	$db_dbname = "forest-platform";
 
 	// mysqlへの接続
 	$mysqli = new mysqli($db_host, $db_user, $db_password, $db_dbname, $db_port);
