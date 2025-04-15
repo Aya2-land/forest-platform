7eee3f7872afb987f63cd3637a85cdb07990817d
sayakakawa <146063396+sayakakawa@users.noreply.github.com>
2025-04-15 16:29:14 +0900

    DB(共有用)

--- kii-system/php/connect_db.php
+++ kii-system/php/connect_db.php
@@ -1,11 +1,11 @@
 <?php
 
 //   各々のローカル
-	// $db_host = "10.240.187.201";  // ホスト名のみ
-	// $db_port = 8889;  // ポート番号
-	// $db_user = "root";
-	// $db_password = "your_password";
-	// $db_dbname = "forest_platform";
+	$db_host = "10.240.187.201";  // ホスト名のみ
+	$db_port = 8889;  // ポート番号
+	$db_user = "root";
+	$db_password = "your_password";
+	$db_dbname = "forest_platform";
 	
 	// 実験データ参照用
 	// $db_host = "localhost:8889";  // DBサーバのurl
@@ -14,11 +14,11 @@
 	// $db_dbname = "FCR-Experiment-Phase1";
 
 	//アプリケーションサーバーにアップロードするとき．
-	$db_host = "localhost";  // DBサーバのurl
-	$db_port = 3306;
-	$db_user = "root";
-	$db_password = "kslabkslab";
-	$db_dbname = "forest_platform";
+	// $db_host = "localhost";  // DBサーバのurl
+	// $db_port = 3306;
+	// $db_user = "root";
+	// $db_password = "kslabkslab";
+	// $db_dbname = "forest_platform";
 
 	// 　統合環境1
 	// $db_host = "192.168.0.82:3306";  // DBサーバのurl
